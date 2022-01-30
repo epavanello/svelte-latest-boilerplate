b0e4d265f34d24a224377c831d09ad7787eb52a8
Emanuele Pavanello <47756116+epavanello@users.noreply.github.com>
2022-01-30 17:09:20 +0000

    env variables and file structure

--- src/containers/Home.svelte
+++ src/containers/Home.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import Button from '$components/Button.svelte'
+
+  let example = import.meta.env.VITE_EXAMPLE
 </script>
 
 <main class="container mx-auto py-20">
   <p class="italic text-center">Home content here</p>
   <Button on:click={() => alert('hi')}>Say hi</Button>
   <Button on:click={() => alert('Emanuele')} type="secondary">To me</Button>
+  <p>
+    Your env variable: {example}
+  </p>
 </main>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_EXAMPLE: string
+    // more env variables...
+}
